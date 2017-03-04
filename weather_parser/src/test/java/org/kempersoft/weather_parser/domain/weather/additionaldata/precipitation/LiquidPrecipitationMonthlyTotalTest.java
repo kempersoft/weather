ed1f7afb1798afd6e34fca6176639f9b307b6ba9
bobkemper <rpkemper@gmail.com>
2017-03-04 11:50:56 -0500

    Test stubs

---
+++ weather_parser/src/test/java/org/kempersoft/weather_parser/domain/weather/additionaldata/precipitation/LiquidPrecipitationMonthlyTotalTest.java
@@ -0,0 +1,16 @@
+package org.kempersoft.weather_parser.domain.weather.additionaldata.precipitation;
+
+import org.junit.Test;
+
+/**
+ * Created by bkemper on 3/4/17.
+ */
+public class LiquidPrecipitationMonthlyTotalTest {
+
+
+  @Test
+  public void parseLine() throws Exception {
+
+  }
+
+}
